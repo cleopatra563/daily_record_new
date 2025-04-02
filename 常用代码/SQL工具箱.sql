0a5fac295ab2b3132a03f04b9789f6f0968a5539
cleopatra563 <cleopatrazhou563@gmail.com>
2025-04-02 10:16:05 +0800

    dd004

--- 常用代码/SQL工具箱.sql
+++ 常用代码/SQL工具箱.sql
@@ -2,10 +2,16 @@
 1、表和字段，互相关系
 2、写意义的注释，有意义的列名称，有意义的表别名
 
-核心概念、方法论
+核心概念&方法论
 1、什么是有序数据、状态流转、操作路径
-前后顺序 上下文  稍微有点偏了
+连续登录天数、计算事件间间隔、检测特定模式（事件A->事件B->事件C）
+分组排序和排名
+生产 质检 出库
+login->建筑升级->人物增强->玩家战斗->下线退出
 
+
+核心原理&执行原理&底层原理
+group by 分组排序和排名->分配行号
 
 实战作业
 1、表结构设计
@@ -14,3 +20,26 @@
 4、输出结果
 
 最佳实践与避坑指南
+
+/*连续天数中*/
+login_date - row_number() over(partition by order by "#event_time")
+with login_group(
+ select role_id,login_date,login_date - row_number() over(partition by role_id order by time) as grp 输出行号辅助列
+ from daily_login
+),
+ consecutive_logins(
+select role_id,grp,
+count(*) as consecutive_days
+min(login_date) as start_date
+max(login_date)
+from login_group
+group by role_id,grp
+having count(*)>=3  --连续登录3天以上
+ )
+select role_id,start_date,end_date,consecutive_days
+from consecutive_logins
+order by role_id,start_date;
+
+ role1 2023-12-20-2 2023-12-18
+ role1 2023-12-19-1 2023-12-18
+ role1 2023-12-21-3 2023-12-18
